a110892bacd597d2f224e94f252b97c8d44f864d
Peiwen Lu <lvpw01@shishike.com>
2016-05-27 17:12:11 +0800

    taskID: 5021 workHour: 0.5 remainHour: 6 comment: Add click mask for counter buttons

--- src/component/mui/counter.jsx
+++ src/component/mui/counter.jsx
@@ -29,15 +29,11 @@ module.exports = React.createClass({
     const { count, step, maximum, minimum } = this.props;
     return (
       <div className="counter">
-        <a
-          className={classnames('btn-minus counter-minus', { 'btn-disabled':!minimum || count === minimum })}
-          onTouchTap={evt => this.onBtnsClick('minus', count - step)}
-        >
+        <a className={classnames('btn-minus counter-minus', { 'btn-disabled':!minimum || count === minimum })}>
+          <span className="counter-click-mask" onTouchTap={evt => this.onBtnsClick('minus', count - step)} />
         </a>
-        <a
-          className={classnames('btn-add counter-add', { 'btn-disabled':!maximum || count === maximum })}
-          onTouchTap={evt => this.onBtnsClick('add', count + step)}
-        >
+        <a className={classnames('btn-add counter-add', { 'btn-disabled':!maximum || count === maximum })}>
+          <span className="counter-click-mask" onTouchTap={evt => this.onBtnsClick('add', count + step)} />
         </a>
         <span className="counter-num">{count}</span>
       </div>
