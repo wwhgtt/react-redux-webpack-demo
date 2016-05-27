f425cfd605c6fa10c14b9cb87ff94f47b52d0689
Peiwen Lu <lvpw01@shishike.com>
2016-05-27 18:57:56 +0800

    taskID: 5029 workHour: 0.5 remainHour: 6 comment: Integrate tiny-cart styles

--- src/component/dish-menu/cart/tiny-cart.jsx
+++ src/component/dish-menu/cart/tiny-cart.jsx
@@ -1,5 +1,7 @@
 const React = require('react');
 
+require('./tiny-cart.scss');
+
 module.exports = React.createClass({
   displayName: 'TinyCart',
   propTypes: {
@@ -12,9 +14,16 @@ module.exports = React.createClass({
     const { onBillBtnTap, onCartIconTap } = this.props;
     return (
       <div className="tiny-cart">
-        <a href="" className="cart-icon" onTouchTap={onCartIconTap}></a>
-        <span className="total-price">28</span>
-        <a href="" className="bill-btn" onTouchTap={onBillBtnTap}>选好啦</a>
+        <div className="tiny-cart-left">
+          <a href="" className="cart-icon" onTouchTap={onCartIconTap}></a>
+          <span className="total-price">28 <small>另有配送费8元</small></span>
+          {/* <span className="tiny-cart-text">购物车是空的</span> */}
+        </div>
+        <div className="tiny-cart-right">
+          {/* <span className="tiny-cart-text">商户已打烊</span> */}
+          {/* <span className="tiny-cart-text">差5元起送</span> */}
+          <a href="" className="tiny-cart-btn btn--yellow" onTouchTap={onBillBtnTap}>选好啦</a>
+        </div>
       </div>
     );
   },
