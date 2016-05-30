5e09c3eeae0a7c3aa3fed2ca1f970e791258a485
Peiwen Lu <lvpw01@shishike.com>
2016-05-30 09:36:11 +0800

    taskID: 5029 workHour: 0.5 remainHour: 5 comment: Optimise tiny-cart-price styles

--- src/component/dish-menu/cart/tiny-cart.jsx
+++ src/component/dish-menu/cart/tiny-cart.jsx
@@ -15,8 +15,8 @@ module.exports = React.createClass({
     return (
       <div className="tiny-cart">
         <div className="tiny-cart-left">
-          <a href="" className="cart-icon" onTouchTap={onCartIconTap}></a>
-          <span className="total-price">28.22 <small>另有配送费8元</small></span>
+          <a href="" className="tiny-cart-icon" onTouchTap={onCartIconTap} data-count="3"></a>
+          <span className="tiny-cart-price"><strong>28.22</strong><small>另有配送费8元</small></span>
           {/* <span className="tiny-cart-text">购物车是空的</span> */}
         </div>
         <div className="tiny-cart-right">
