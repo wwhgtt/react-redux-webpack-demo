7fe0531b2b0a027c2f07c585485db33f78a285e8
Peiwen Lu <lvpw01@shishike.com>
2016-06-01 16:17:31 +0800

    taskID: 5029 workHour: 1 remainHour: 5 comment: Integrate expand-cart styles

--- src/component/dish-menu/cart/expand-cart.jsx
+++ src/component/dish-menu/cart/expand-cart.jsx
@@ -1,5 +1,7 @@
 const React = require('react');
 
+require('./expand-cart.scss');
+
 module.exports = React.createClass({
   displayName: 'ExpandCart',
   propTypes: {
@@ -20,17 +22,27 @@ module.exports = React.createClass({
     const cartOrderedList = this.buildCartOrderedList(orderedDishesData);
     return (
       <div className="expand-cart">
-        <div className="headbar">
-          <a href="" className="cart-icon" onTouchTap={onCartIconTap} data-count={dishCount}></a>
-          <a className="clear-cart-btn"></a>
-        </div>
-        {cartOrderedList}
-        <div className="bottombar">
-          {
-            dishCount === 0 ? <span className="cart-text">购物车是空的</span> :
-              <span className="cart-price price"><strong>{totalPrice}</strong><small>另有配送费8元</small></span>
-          }
-          <a href="" className="tiny-cart-btn btn--yellow" onTouchTap={onBillBtnTap}>选好啦</a>
+        <div className="expand-cart-main">
+          <div className="expand-cart-header">
+            <a href="" className="cart-icon cart-icon--expand" onTouchTap={onCartIconTap} data-count={dishCount}></a>
+            <a className="expand-cart-clear">清空购物车</a>
+          </div>
+
+          {cartOrderedList}
+
+          <div className="tiny-cart">
+            <div className="tiny-cart-left">
+              {
+                dishCount === 0 ? <span className="tiny-cart-text">购物车是空的</span> :
+                <span className="tiny-cart-price price"><strong>{totalPrice}</strong><small>另有配送费8元</small></span>
+              }
+            </div>
+            <div className="tiny-cart-right">
+              {/* <span className="tiny-cart-text">商户已打烊</span> */}
+              {/* <span className="tiny-cart-text">差5元起送</span> */}
+              <a href="" className="tiny-cart-btn btn--yellow" onTouchTap={onBillBtnTap}>选好啦</a>
+            </div>
+          </div>
         </div>
       </div>
     );
