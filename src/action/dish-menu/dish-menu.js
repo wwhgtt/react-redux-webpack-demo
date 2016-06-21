0366049e7efb344f5527a72d113531b2aed7f77f
Peiwen Lu <lvpw01@shishike.com>
2016-06-21 10:37:28 +0800

    taskID: 5022
    workHour: 3
    remainHour: 5
    comment: Save last selected dishes to localstorage

--- src/action/dish-menu/dish-menu.js
+++ src/action/dish-menu/dish-menu.js
@@ -4,6 +4,8 @@ require('es6-promise');
 require('isomorphic-fetch');
 const helper = require('../../helper/dish-hepler');
 const setMenuData = createAction('SET_MENU_DATA', menuData => menuData);
+const orderDishAction = createAction('ORDER_DISH', (dishData, action) => [dishData, action]);
+const removeAllDishesAction = createAction('REMOVE_ALL_DISHES');
 exports.showDishDetail = createAction('SHOW_DISH_DETAIL', dishData => dishData);
 exports.orderDish = createAction('ORDER_DISH', (dishData, action) => [dishData, action]);
 exports.removeAllOrders = createAction('REMOVE_ALL_ORDERS', orders => orders);
@@ -35,12 +37,43 @@ exports.fetchMenuData = () => (dispatch, getStates) => {
       return res.json();
     }).
     then(menuData => {
+      if (localStorage.getItem('lastShopId') === helper.getUrlParam('shopId')) {
+        const lastSelectedDishes = JSON.parse(localStorage.getItem('lastSelectedDishes'));
+        menuData.data.dishList.forEach(dish => {
+          if (lastSelectedDishes[dish.id]) {
+            dish.order = lastSelectedDishes[dish.id];
+          }
+        });
+      } else {
+        localStorage.removeItem('lastShopId');
+        localStorage.removeItem('lastSelectedDishes');
+      }
       dispatch(setMenuData(menuData.data));
     }).
     catch(err => {
       throw err;
     });
 };
+
+exports.orderDish = (dishData, action) => (dispatch, getStates) => {
+  let lastSelectedDishes = {};
+
+  dispatch(orderDishAction(dishData, action));
+
+  helper.getOrderedDishes(getStates().dishesData).forEach(dish => {
+    lastSelectedDishes[dish.id] = dish.order;
+  });
+
+  localStorage.setItem('lastShopId', helper.getUrlParam('shopId'));
+  localStorage.setItem('lastSelectedDishes', JSON.stringify(lastSelectedDishes));
+};
+
+exports.removeAllDishes = () => (dispatch, getStates) => {
+  localStorage.removeItem('lastShopId');
+  localStorage.removeItem('lastSelectedDishes');
+  dispatch(removeAllDishesAction());
+};
+
 exports.setDishCookie = () => (dispatch, getStates) => {
   const dishesData = getStates().dishesData;
   const orderedData = helper.getOrderedDishes(dishesData);
