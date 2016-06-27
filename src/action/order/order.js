1d32cd7694a38fd4fec08f7a2971df5d30799380
wenhai wu <tubage2hao@gmail.com>
2016-06-27 17:43:25 +0800

    taskID:5500 workHour:0.5 remainHour:1.5 comment:fix bugs

--- src/action/order/order.js
+++ src/action/order/order.js
@@ -4,7 +4,7 @@ require('es6-promise');
 require('isomorphic-fetch');
 const setOrder = createAction('SET_ORDER', menuData => menuData);
 exports.fetchOrder = () => (dispatch, getState) => {
-  fetch(config.orderDineInMenuAPi, {
+  fetch(config.orderDineInAPi, {
     method: 'GET', mod: 'cors',
     headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
   }).
