945a15a29862a8ab1a30fa00d2c1db456e6838a0
wenhai wu <tubage2hao@gmail.com>
2016-06-28 09:36:39 +0800

    taskID:5563 workHour:0.5 remainHour:3.5 comment:reconstruct state

--- src/reducer/order/index.js
+++ src/reducer/order/index.js
@@ -1,12 +1,28 @@
 const Immutable = require('seamless-immutable');
 module.exports = function (
-  state = Immutable.from({ order:undefined }),
+  state = Immutable.from({
+    areaList:[],
+    tableList:[],
+    timeJson:{},
+    diningerProps:{},
+    commercialProps:{},
+  }),
   action
 ) {
   const { type, payload } = action;
   switch (type) {
-    case 'SET_ORDER':
-      return state.setIn(['order'], payload);
+    case 'SET_ORDER': {
+      const diningerProps = Object.assign({}, { name:payload.name }, { sex:payload.sex },
+      { isMember:payload.isMember }, { serviceApproach:payload.serviceApproach });
+      const commercialProps = Object.assign({}, { name:payload.commercialName }, { integral:payload.integral },
+      { commercialLogo:payload.commercialLogo }, { pickupPayType:payload.pickupPayType },
+      { totablePayType:payload.totablePayType });
+      return state.setIn(['areaList'], payload.areaList)
+                  .setIn(['tableList'], payload.tableList)
+                  .setIn(['timeJson'], payload.timeJson)
+                  .setIn(['diningerProps'], diningerProps)
+                  .setIn(['commercialProps'], commercialProps);
+    }
     default:
       return state;
   }
