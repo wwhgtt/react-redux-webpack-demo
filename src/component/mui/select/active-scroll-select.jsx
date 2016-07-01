3ac9dada39b88fa8bb87c966fd4d894fe4bab72b
Peiwen Lu <lvpw01@shishike.com>
2016-07-01 15:48:39 +0800

    taskID: 5757
    workHour: 1
    remainHour: 10
    comment: Integarte active-scroll-select styles

--- src/component/mui/select/active-scroll-select.jsx
+++ src/component/mui/select/active-scroll-select.jsx
@@ -3,6 +3,10 @@ const { findDOMNode } = require('react-dom');
 const classnames = require('classnames');
 const IScroll = require('iscroll/build/iscroll-probe');
 const ActiveSelect = require('./active-select.jsx');
+
+require('../../../asset/style/style.scss');
+require('./active-scroll-select.scss');
+
 module.exports = React.createClass({
   displayName: 'ActiveScrollSelect',
   propTypes: {
@@ -18,8 +22,13 @@ module.exports = React.createClass({
     const { className, ...props } = this.props;
     return (
       <div className={classnames('scroll-select', className)}>
-        <div className="scroll-wrapper">
-          <ActiveSelect {...props} />
+        <div className="scroll-select-header">
+          <span>选择地区</span>
+          <button className="scroll-select-confirm btn--yellow">确定</button>
+        </div>
+        <div className="scroll-select-content flex-row indicator-box">
+          <ActiveSelect className="scroll-select-column flex-rest" {...props} />
+          <ActiveSelect className="scroll-select-column flex-rest" {...props} />
         </div>
       </div>
     );
