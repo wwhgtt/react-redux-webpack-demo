3742eccf248934be018c3e0ff328e4ef728bf89d
wenhai wu <tubage2hao@gmail.com>
2016-07-29 09:21:05 +0800

    taskID:6167 workHour:1 remainHour:3 comment:fix address

--- dyson_services/get/user-address.js
+++ dyson_services/get/user-address.js
@@ -1,16 +1,7 @@
 module.exports = {
   path:'/user/addressList.json',
   template:{
-    data: [
-      {
-        id: 262910,
-        address: '北京市北京市东城区555',
-      },
-      {
-        id: 262193,
-        address: '北京市北京市东城区你来过',
-      },
-    ],
+    data: [],
     msg: '',
     time: 1467811619403,
     code: '200',
