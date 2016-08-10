273288f210880535363476338cd46c9dcff72694
Peiwen Lu <lvpw01@shishike.com>
2016-08-10 15:24:22 +0800

    taskID: 6488
    workHour: 1
    remainHour: 20
    comment: Add customer address select form styles

--- src/component/customer-address/customer-address-editor.jsx
+++ src/component/customer-address/customer-address-editor.jsx
@@ -66,24 +66,22 @@ module.exports = React.createClass({
   render() {
     const { customerProps } = this.props;
     return (
-      <div className="order-subpage">
-        <div className="order-subpage-content">
-          <div className="options-group">
-            <div className="order-prop-option">
-              <span className="option-title">姓名：</span>
-              <div className="editor-input">
-                <input
-                  type="text"
-                  className="editor-input"
-                  name="name"
-                  id="editor-name"
-                  placeholder="请录入姓名"
-                  maxLength="30"
-                  value={customerProps.name || ''}
-                  onChange={this.handleBasicInfoChange}
-                />
-              </div>
-              <div className="" style={{ float: 'left' }}>
+      <div className="customer-address-page">
+        <div className="options-group">
+          <div className="option">
+            <span className="option-title">姓名：</span>
+            <div className="option-content">
+              <input
+                type="text"
+                className="option-input customer-name"
+                name="name"
+                id="editor-name"
+                placeholder="请录入姓名"
+                maxLength="30"
+                value={customerProps.name || ''}
+                onChange={this.handleBasicInfoChange}
+              />
+              <div className="customer-gender">
                 <label className="half">
                   <input
                     className="option-radio" type="radio" name="sex" defaultValue="1"
@@ -102,52 +100,50 @@ module.exports = React.createClass({
                 </label>
               </div>
             </div>
-
-            <label className="order-prop-option">
-              <span className="option-title">联系电话：</span>
-              <input
-                type="number"
-                className="editor-input"
-                name="mobile"
-                onChange={this.handleBasicInfoChange}
-                maxLength="11"
-                value={customerProps.mobile || ''}
-              />
-            </label>
-            <label className="order-prop-option">
-              <span className="option-title">收货地址：</span>
-              <input
-                type="text"
-                className="editor-input"
-                name="baseAddress"
-                onChange={this.handleBasicInfoChange}
-                maxLength="35"
-                value={customerProps.baseAddress || ''}
-              />
-              <a className="option-btn btn-arrow-right" href="#address-select"></a>
-            </label>
-            <label className="order-prop-option">
-              <span className="option-title">门牌信息：</span>
-              <input
-                type="text"
-                className="editor-input"
-                name="street"
-                onChange={this.handleBasicInfoChange}
-                maxLength="35"
-                value={customerProps.street || ''}
-              />
-            </label>
           </div>
 
-          {customerProps.id ?
-            <div className="options-group">
-              <a className="order-prop-option address-delete-text" onTouchTap={this.onRemoveLinktap}>删除地址</a>
-            </div>
-            :
-            false
-          }
+          <label className="option">
+            <span className="option-title">联系电话：</span>
+            <input
+              type="tel"
+              className="option-content option-input"
+              name="mobile"
+              onChange={this.handleBasicInfoChange}
+              maxLength="11"
+              placeholder="请输入手机号码"
+              value={customerProps.mobile || ''}
+            />
+          </label>
+          <label className="option">
+            <span className="option-title">收货地址：</span>
+            <a className="option-content btn-arrow-right customer-address" href="#address-select">
+              <span className="customer-address-text ellipsis">{customerProps.baseAddress || ''}</span>
+              <span className="customer-address-placeholder">请选择收货地址</span>
+            </a>
+          </label>
+          <label className="option">
+            <span className="option-title">门牌信息：</span>
+            <input
+              type="text"
+              className="option-content option-input"
+              name="street"
+              onChange={this.handleBasicInfoChange}
+              maxLength="35"
+              placeholder="请填写门牌号码"
+              value={customerProps.street || ''}
+            />
+          </label>
         </div>
-        <button className="order-subpage-submit btn--yellow" onTouchTap={this.onSaveBtntap}>保存</button>
+
+        {customerProps.id ?
+          <div className="options-group">
+            <button className="option customer-address-delete" onTouchTap={this.onRemoveLinktap}>删除地址</button>
+          </div>
+          :
+          false
+        }
+
+        <button className="btn-submit btn--yellow" onTouchTap={this.onSaveBtntap}>保存</button>
       </div>
     );
   },
