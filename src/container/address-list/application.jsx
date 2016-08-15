2a07e8981905fd6ed7413922075aee76b8112954
hengjun chen <chj@StephenGao-2.local>
2016-08-15 14:32:06 +0800

    taskID:6051 workHour:1 remainHour:2 comment:修改地址编辑界面

--- src/container/address-list/application.jsx
+++ src/container/address-list/application.jsx
@@ -2,8 +2,11 @@ const React = require('react');
 const connect = require('react-redux').connect;
 const actions = require('../../action/customer-address/customer-address');
 const getUrlParam = require('../../helper/dish-hepler.js').getUrlParam;
-const AddresList = require('../../component/address-list/address-list.jsx');
+const ActiveSelect = require('../../component/mui/select/active-select.jsx');
+const CustomerAddressOption = require('../../component/order/customer-address-option.jsx');
 const shopId = getUrlParam('shopId') || '';
+
+require('../../component/order/customer-takeaway-info-editor.scss');
 require('../../asset/style/style.scss');
 require('./application.scss');
 const AddressListApplication = React.createClass({
@@ -14,20 +17,77 @@ const AddressListApplication = React.createClass({
     // MapedStatesToProps
     allAddressList: React.PropTypes.array,
   },
+  getInitialState() {
+    return {
+      allAddressList: [],
+    };
+  },
   componentDidMount() {
     const { fetchAllAddressList } = this.props;
     fetchAllAddressList(shopId);
+  },
+  componentWillReceiveProps(newProps) {
+    this.initStateByProps(newProps);
   },
   onAddressEditor(editor, option) {
     const { setSessionAndForwardEditUserAddress } = this.props;
     setSessionAndForwardEditUserAddress(shopId, editor);
   },
+  onAddressSelect(evt, option, func) {
+    const editor = evt.target.getAttribute('data-editor');
+    this.onAddressEditor(editor, option);
+  },
+  initStateByProps(props) {
+    const { allAddressList } = props;
+    if (!allAddressList) {
+      return;
+    }
+
+    this.setState({
+      allAddressList,
+    });
+  },
+  buildAddressElement() {
+    const { allAddressList } = this.state;
+    const elems = [];
+    const addressListToOptionsData = addressList => addressList.map(item => {
+      const { address, name, sex, mobile } = item;
+      return {
+        id: item.id,
+        address,
+        label: address,
+        name,
+        mobile,
+        isChecked: item.isChecked,
+        sex: sex === 1 ? '先生' : '女士 ',
+      };
+    });
+    if (allAddressList.length) {
+      elems.push(
+        <ActiveSelect
+          key="inSelect"
+          className="address-group"
+          optionsData={addressListToOptionsData(allAddressList)}
+          optionComponent={CustomerAddressOption}
+          onSelectOption={this.onAddressSelect}
+        />
+      );
+    }
+    if (allAddressList.length < 10) {
+      elems.push(<a key="add" className="address-add-more" onTouchTap={this.onAddressSelect}>增加地址</a>);
+    }
+    return elems;
+  },
   render() {
     return (
-      <AddresList
-        allAddressList={this.props.allAddressList || []}
-        onAddressEditor={this.onAddressEditor}
-      />
+      <div className="order-subpage">
+        <div className="order-subpage-content">
+          {this.buildAddressElement()}
+          <div className="address-title address-count-descript">
+            最多为您保存10个常用地址，还需要新增，请删除或修改以上地址
+          </div>
+        </div>
+      </div>
     );
   },
 });
