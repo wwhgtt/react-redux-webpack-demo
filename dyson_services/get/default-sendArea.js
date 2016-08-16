7fcbd45847a772881674e14bb7c09c313a5edb19
Peiwen Lu <lvpw01@shishike.com>
2016-08-16 15:17:55 +0800

    taskID: 6488
    workHour: 0.5
    remainHour: 20
    comment: Rename sendAreaId to id

--- dyson_services/get/default-sendArea.js
+++ dyson_services/get/default-sendArea.js
@@ -1,14 +1,22 @@
 module.exports = {
   path: '/user/getDefaultSendArea.json',
-  template:{
+  template: {
     data: {
-      sendAreaId: 23332,
-      sendPrice: 15,
-      shipment: 5,
-      freeDeliveryPrice: 25,
+      id: 124,
+      commercialId: 810006427,
+      name: '8u7',
+      sendPrice: 2.0,
+      shipment: 2.0,
+      freeDeliveryPrice: 70.0,
+      sendTime: 10,
+      status: true,
+      sort: 0,
+      createTime: 1471316783000,
+      modifyTime: null,
+      existId: true,
     },
+    time: 1471330563607,
+    code: 200,
     msg: '',
-    time: 1470648360793,
-    code: '200',
   },
 };
