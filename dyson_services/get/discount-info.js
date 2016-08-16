3d510000588ef9972cc59e6324097c4259117d99
wenhai wu <tubage2hao@gmail.com>
2016-08-16 16:01:47 +0800

    taskID:6533 workHour:1 remainHour:2 comment:fix memberprice

--- dyson_services/get/discount-info.js
+++ dyson_services/get/discount-info.js
@@ -7,16 +7,7 @@ module.exports = {
       levelId:9992836,
       levelName:'黄金会员',
       isMember:false,
-      dishList:[
-        {
-          dishId:57472,
-          value:8,
-        },
-        {
-          dishId:61,
-          value:9,
-        },
-      ],
+      dishList:null,
     },
     time :1467713952401,
     code : 200,
