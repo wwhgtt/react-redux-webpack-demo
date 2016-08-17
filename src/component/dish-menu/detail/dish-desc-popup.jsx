51ec28e4f510a694b748fb5aa9860251255e10d7
Peiwen Lu <lvpw01@shishike.com>
2016-08-17 09:40:05 +0800

    taskID: 6488
    workHour: 0.5
    remainHour: 20
    comment: Add flex layout for dish-details-popup

--- src/component/dish-menu/detail/dish-desc-popup.jsx
+++ src/component/dish-menu/detail/dish-desc-popup.jsx
@@ -35,31 +35,33 @@ module.exports = React.createClass({
     return (
       <div className="dish-detail-container">
         <a className="dish-detail-close" onTouchTap={this.onCloseBtnTap}></a>
-        <div className="dish-detail-content dish-detail-content--white">
-          <img className="dish-desc-image" src={dish.largeImgUrl || imagePlaceholder} alt="" />
-          <div className="dish-desc-content">
-            <h2 className="dish-desc-title">{helper.generateDishNameWithUnit(dish)}</h2>
-            {dish.isMember ?
-              <p className="clearfix">
-                <span className="dish-desc-price--del price">{dish.marketPrice}</span>
-                <span className="dish-desc-price-title">会员价:</span>
-                <span className="dish-desc-price--bold price">{memberPrice.toFixed(2)}</span>
-                <span className="dish-desc-price-badge">{dish.discountLevel}专享价</span>
-              </p>
-              : <p className="clearfix"><span className="dish-desc-price--bold price">{dish.marketPrice}</span></p>
-            }
-            <h3 className="dish-desc-subtitle">美食简介</h3>
-            <p className="dish-desc-desc">{dish.dishDesc}</p>
+        <div className="dish-detail-content dish-detail-content--white flex-columns">
+          <div className="dish-desc-content flex-rest">
+            <img className="dish-desc-image" src={dish.largeImgUrl || imagePlaceholder} alt="" />
+            <div className="dish-desc-info">
+              <h2 className="dish-desc-title">{helper.generateDishNameWithUnit(dish)}</h2>
+              {dish.isMember ?
+                <p className="clearfix">
+                  <span className="dish-desc-price--del price">{dish.marketPrice}</span>
+                  <span className="dish-desc-price-title">会员价:</span>
+                  <span className="dish-desc-price--bold price">{memberPrice.toFixed(2)}</span>
+                  <span className="dish-desc-price-badge">{dish.discountLevel}专享价</span>
+                </p>
+                : <p className="clearfix"><span className="dish-desc-price--bold price">{dish.marketPrice}</span></p>
+              }
+              <h3 className="dish-desc-subtitle">美食简介</h3>
+              <p className="dish-desc-desc">{dish.dishDesc}</p>
+            </div>
           </div>
+          {!dish.isUserMember ?
+            <div className="dish-desc-register flex-none">
+              <span className="dish-desc-register-text">注册会员，享受惊喜价格哦！</span>
+              <a className="dish-desc-register-btn" href={`http://${location.host}/member/register?shopId=${helper.getUrlParam('shopId')}`}>注册会员</a>
+            </div>
+            :
+            <div className="dish-desc-register flex-none"></div>
+          }
         </div>
-        {!dish.isUserMember ?
-          <div className="dish-desc-register">
-            <span className="dish-desc-register-text">注册会员，享受惊喜价格哦！</span>
-            <a className="dish-desc-register-btn" href={`http://${location.host}/member/register?shopId=${helper.getUrlParam('shopId')}`}>注册会员</a>
-          </div>
-          :
-          <div className="dish-desc-register"></div>
-        }
       </div>
     );
   },
