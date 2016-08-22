da7db95f2e6d2f4987b00fe0ecfda20bfdca973e
wenhai wu <tubage2hao@gmail.com>
2016-08-22 17:13:22 +0800

    taskID:6597 workHour:1 remainHour:2 comment:add test

--- test/unit/order-helper.spec.js
+++ test/unit/order-helper.spec.js
@@ -1,5 +1,5 @@
 const orderHelper = require('../../src/helper/order-helper.js');
-
+const integralProps = require('../data/intergral.js');
 describe('Helper', function () {
   describe('Order Related Helper', function () {
     it('isEmptyObject', function () {
@@ -10,6 +10,12 @@ describe('Helper', function () {
     it('getSelectedTable', function () {
       orderHelper.getSelectedTable({ areas:[{ isChecked:true, id:100 }], tables:[{ isChecked:true, id:200 }] })
         .should.deep.equal({ area:{ isChecked:true, id:100 }, table:{ isChecked:true, id:200 } });
+    });
+
+    it('countIntegralsToCash', function () {
+      console.log(integralProps);
+      orderHelper.countIntegralsToCash(100, integralProps)
+        .should.deep.equal({ commutation:96, integralInUsed:276 });
     });
   });
 });
