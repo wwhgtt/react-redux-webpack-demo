044dc24b6cf6765a464a50f7b15c9382be0b156d
yangdan <dan@dandeMacBook-Pro.local>
2016-08-25 00:07:02 +0800

    首页样式调整

--- src/component/bind-account/bind-phone-index.js
+++ src/component/bind-account/bind-phone-index.js
@@ -1,15 +1,16 @@
 import React from 'react';
+require('./bind-account.scss');
 
 class BindPhoneIndex extends React.Component {
 	render() {
 		return (
-			<div>
-				<div>图片</div>
-				<div>
-					<p>您还没有绑定手机号</p>
-					<p>绑定手机号码可以关联原有的手机号信息</p>
-					<a href="#bind-validate">绑定手机号</a>
+			<div className="bind-account">
+				<div className="account-info">
+					<div className="account-phone-img">图片</div>
+					<p className="account-info-state">您还没有绑定手机号</p>
+					<p className="account-info-tips">绑定手机号码可以关联原有的手机号信息</p>
 				</div>
+				<a className="btn account-btn btn--yellow" href="#bind-validate">绑定手机号</a>
 			</div>
 		)
 	}
