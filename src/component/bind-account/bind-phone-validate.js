36c7e46f661e8b61410ad8446e46c655b7cee98c
yang dan <yangdan@macdeMacBook-Pro-4.local>
2016-08-30 12:03:08 +0800

    taskID: 6726 workHour: 1 remainHour: 999 comment: 测试

--- src/component/bind-account/bind-phone-validate.js
+++ src/component/bind-account/bind-phone-validate.js
@@ -20,7 +20,7 @@ const BindPhoneValidate = React.createClass({
 
   handleClick(e) {
     const { phoneNum, phoneCode } = this.state || {};
-    console.log(this.state);
+    // console.log(this.state);
     if (!phoneNum) {
       this.setState({ showMsg: true });
       return;
