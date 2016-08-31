a49abdfbdec9834a1f1eea193ddda9e35f2d6b70
AJ Frank <xuhj@shishike.com>
2016-08-31 18:05:56 +0800

    合并杨丹

---
+++ dyson_services/get/sendCode.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   path:'/user/sendCode.json',
+//   template:{
+//     data: {
+//       status: 0,
+//       message: '短信验证码发送成功',
+//     },
+//     time: 1472439316794,
+//     code: '',
+//     msg: '未登录',
+//   },
+// };
