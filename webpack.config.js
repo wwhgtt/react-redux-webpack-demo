293c7db29841ac64079a5c5f7b308d35d057f1a9
wenhai wu <tubage2hao@gmail.com>
2016-08-31 10:27:35 +0800

    taskID:6621 workHour:1 remainHour:2 comment:add order -in -line

--- webpack.config.js
+++ webpack.config.js
@@ -30,9 +30,6 @@ module.exports = {
     'order-inLine': [
       './src/order-inLine.jsx',
     ],
-    'place-order': [
-      './src/place-order.jsx',
-    ],
   },
   resolve: {
     fallback: '/usr/local/lib/node_modules',
@@ -130,14 +127,6 @@ module.exports = {
         title: 'OrderInLineApplication',
         filename: 'order-inLine.html',
         chunks: ['common', 'order-inLine'],
-        inject: 'body', template: './src/helper/html-webpack-plugin-template.html',
-      }
-    ),
-    new HtmlWebpackPlugin(
-      {
-        title: 'PlaceOrderApplication',
-        filename: 'place-order.html',
-        chunks: ['common', 'place-order'],
         inject: 'body', template: './src/helper/html-webpack-plugin-template.html',
       }
     ),
