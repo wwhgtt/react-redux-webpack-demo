a5719fc5bcc52cd3be99674f8ee1ddb23d9879e2
AJ Frank <xuhj@shishike.com>
2016-09-04 18:04:37 +0800

    性别判断

--- src/action/mine/mine-setting.js
+++ src/action/mine/mine-setting.js
@@ -59,7 +59,7 @@ exports.updateInfo = (name, sex, condition) => (dispatch, getStates) => {
   if (!shopId) {
     dispatch(setErrorMsg('找不到门店号'));
     return;
-  } else if (!sex.toString()) {
+  } else if (sex.toString() !== '0' && sex.toString() !== '1') {
     dispatch(setErrorMsg('请选择性别!!'));
     return;
   } else if (!name.replace(/(^\s+)|(\s+$)/g, '')) {
