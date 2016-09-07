c4e701b4501d42a1accd80bab0ea7205b3b665c0
wenhai wu <wuwh@shishike.com>
2016-09-07 18:01:53 +0800

    taskID:6937 workHour:1 remainHour:2 comment:fix

--- src/component/order/customer-info-editor.jsx
+++ src/component/order/customer-info-editor.jsx
@@ -32,52 +32,46 @@ module.exports = React.createClass({
     const { isMobileDisabled } = this.props;
     return (
       <div className="editor options-group">
-        {customerProps ?
-          <div>
-            <div className="option">
-              <div className="flex-row">
-                <span className="editor-title flex-none">姓名：</span>
+        <div className="option">
+          <div className="flex-row">
+            <span className="editor-title flex-none">姓名：</span>
+            <input
+              className="editor-input flex-rest"
+              name="name"
+              id="editor-name"
+              placeholder={customerProps.name || '请输入姓名'}
+              onChange={this.handleBasicInfoChange}
+            />
+            <div className="editor-gender-group flex-none">
+              <label className="half">
                 <input
-                  className="editor-input flex-rest"
-                  name="name"
-                  id="editor-name"
-                  placeholder={customerProps.name || '请输入姓名'}
-                  onChange={this.handleBasicInfoChange}
+                  className="option-radio" type="radio" name="sex" defaultValue="1"
+                  onChange={this.handleBasicInfoChange} checked={customerProps.sex === '1' || customerProps.sex === 1}
                 />
-                <div className="editor-gender-group flex-none">
-                  <label className="half">
-                    <input
-                      className="option-radio" type="radio" name="sex" defaultValue="1"
-                      onChange={this.handleBasicInfoChange} checked={customerProps.sex === '1' || customerProps.sex === 1}
-                    />
-                    <span className="editor-gender">先生</span>
-                  </label>
-                  <label className="half">
-                    <input
-                      className="option-radio" type="radio" name="sex" defaultValue="0"
-                      onChange={this.handleBasicInfoChange} checked={customerProps.sex === '0' || customerProps.sex === 0}
-                    />
-                    <span className="editor-gender">女士</span>
-                  </label>
-                </div>
-              </div>
-            </div>
-
-            <div className="option flex-row">
-              <span className="editor-title flex-none">手机号：</span>
-              <input
-                name="mobile"
-                className="editor-input editor-input--right flex-rest"
-                value={customerProps.mobile ? customerProps.mobile : ''}
-                placeholder={customerProps.mobile || '请输入手机号'}
-                onChange={this.handleBasicInfoChange}
-                disabled={isMobileDisabled}
-              />
+                <span className="editor-gender">先生</span>
+              </label>
+              <label className="half">
+                <input
+                  className="option-radio" type="radio" name="sex" defaultValue="0"
+                  onChange={this.handleBasicInfoChange} checked={customerProps.sex === '0' || customerProps.sex === 0}
+                />
+                <span className="editor-gender">女士</span>
+              </label>
             </div>
           </div>
-          :
-          false
-        }
+        </div>
+
+        <div className="option flex-row">
+          <span className="editor-title flex-none">手机号：</span>
+          <input
+            name="mobile"
+            className="editor-input editor-input--right flex-rest"
+            value={customerProps.mobile ? customerProps.mobile : ''}
+            placeholder={customerProps.mobile || '请输入手机号'}
+            onChange={this.handleBasicInfoChange}
+            disabled={isMobileDisabled}
+          />
+        </div>
       </div>
     );
   },
