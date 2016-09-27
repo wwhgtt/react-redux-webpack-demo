529e1afcab011625d1e1c652faf6f4353ace0a7d
hengjun chen <chenhj@shishike.com>
2016-09-27 16:27:03 +0800

    taskID:6051 workHour:1 remainHour:2 comment:update submit order api

--- dyson_services/post/submit-WM-order.js
+++ dyson_services/post/submit-WM-order.js
@@ -2,10 +2,11 @@ module.exports = {
   path:'/takeaway/subOrder.json',
   template:{
     data: {
-
+      orderId: +new Date(),
     },
     time: 1472525132641,
-    code: '20013', // 20013  表示手机未验证
+    code: '200', // 20013  表示手机未验证
     msg: '验证码错误',
   },
 };
+
