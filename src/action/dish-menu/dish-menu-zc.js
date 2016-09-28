0fe47388dd6339fc0a5d234cdcd60a2aaf01d0d0
AJ Frank <xuhj@shishike.com>
2016-09-28 19:33:14 +0800

    1933

--- src/action/dish-menu/dish-menu-zc.js
+++ src/action/dish-menu/dish-menu-zc.js
@@ -99,9 +99,9 @@ const removeBasicSession = (name) => {
 const errorLocation = (errorCode) => {
   switch (errorCode) {
     case '90006' : location.href = `${config.exceptionLinkURL}?shopId=${shopId}`; break; // 请重新扫描二维码,链接已失效
-    case '90008' : location.href = `${config.exceptionDishURL}?shopId=${shopId}`; break; // 该桌台有多个未支付的正餐订单
-    case '90012' : location.href = `${config.exceptionDishURL}?shopId=${shopId}`; break; // 该用户在该门店下有多个正餐加菜订单
-    case '90013' : location.href = `${config.exceptionDishURL}?shopId=${shopId}`; break; // 该用户在该门店下有多个正餐订单
+    // case '90008' : location.href = `${config.exceptionDishURL}?shopId=${shopId}`; break; // 该桌台有多个未支付的正餐订单
+    // case '90012' : location.href = `${config.exceptionDishURL}?shopId=${shopId}`; break; // 该用户在该门店下有多个正餐加菜订单
+    // case '90013' : location.href = `${config.exceptionDishURL}?shopId=${shopId}`; break; // 该用户在该门店下有多个正餐订单
     case '90014' : location.href = `${config.exceptionDishCurrentURL}?shopId=${shopId}`; break; // 待清台,桌台锁定中,桌台不属于该门店,不存在对应桌台,对应桌台已被删除
     default : break;
   }
@@ -167,7 +167,7 @@ const fetchTableInfo = exports.fetchTableInfo = (tableParam) => (dispatch, getSt
       console.log(err);
     });
 
-// 根据tableID获取基本信息
+// 根据tableId获取基本信息
 const fetchServiceStatus = exports.fetchServiceStatus = (tableParam) => (dispatch, getState) =>
   fetch(`${config.getServiceStatusAPI}?shopId=${helper.getUrlParam('shopId')}&${tableParam}`, config.requestOptions).
     then(res => {
