cdb2e0a1eb885364148cb8be39912ad72ad25cb0
wenhai wu <wuwh@shishike.com>
2016-09-29 16:59:51 +0800

    taskID:6989 workHour:1 remainHour:2 comment:fix

--- src/helper/place-order-helper.js
+++ src/helper/place-order-helper.js
@@ -8,7 +8,7 @@ exports.initializeTableProps = function (tableProps) {
     tableProp => tableProp.tableInfoList.forEach(
       table => {
         table.areaId = parseInt(tableProp.areaId, 10);
-        table.tableName = `${table.pNum}桌(还有${table.count}张)`;
+        table.tableName = `${table.pNum}人桌(还有${table.count}张)`;
         table.id = tableProp.areaId + Math.random();
         tables.push(table);
       }
