6b9209326f16595b99ec5d2ded1c1877b383dfa1
wenhai wu <wuwh@shishike.com>
2016-10-18 10:40:31 +0800

    taskID:7607 workHour:1 remainHour:2 comment:add data

---
+++ dyson_services/get/activity-benefit.js
@@ -0,0 +1,46 @@
+module.exports = {
+  path:'/marketplan/dishPrivilegeList.json',
+  template:{
+    data: {
+      dishPriList: [
+        {
+          dishId: 198, // 菜品ID
+          dishPriInfo: [
+            {
+              priId: 2583, // 优惠ID
+              priName: '满2件减5元', // 优惠名称
+              priType: 2, // 优惠类型（1单商品营销方案2礼品券）
+              priAmount: 5.0, // 优惠后金额
+            },
+            {
+              priId: 2581,
+              priName: '满100送鸡蛋汤',
+              priType: 1,
+              priAmount: 0,
+            },
+          ],
+        },
+        {
+          dishId: 191,
+          dishPriInfo: [
+            {
+              priId: 2583,
+              priName: '满2件减5元',
+              priType: 2,
+              priAmount: 5.0,
+            },
+            {
+              priId: 2581,
+              priName: '满100送鸡蛋汤',
+              priType: 1,
+              priAmount: 0,
+            },
+          ],
+        },
+      ],
+    },
+    time: 1474284552572,
+    code: 200,
+    msg: '',
+  },
+};
