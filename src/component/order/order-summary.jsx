fd2fdbccef6a30a1c23a19910e2b60d49c9305b3
Peiwen Lu <lvpw01@shishike.com>
2016-10-21 17:20:14 +0800

    taskID: 6990
    workHour: 2
    remainHour: 20
    comment: Add order page benefit styles

--- src/component/order/order-summary.jsx
+++ src/component/order/order-summary.jsx
@@ -55,18 +55,19 @@ module.exports = React.createClass({
 
     const orderedElements = this.buildOrderedElements(orderedDishesProps.dishes);
     return (
-      <div className="options-group">
-        {isNeedShopMaterial ?
-          <a className="option option-shop">
-            <img className="option-shop-icon" src={commercialProps.commercialLogo || defaultShopLogo} alt="" />
-            <p className="option-shop-desc ellipsis">{commercialProps.name}</p>
-          </a>
-          :
-          false
-        }
+      <div>
+        <div className="options-group">
+          {isNeedShopMaterial ?
+            <a className="option option-shop">
+              <img className="option-shop-icon" src={commercialProps.commercialLogo || defaultShopLogo} alt="" />
+              <p className="option-shop-desc ellipsis">{commercialProps.name}</p>
+            </a>
+            :
+            false
+          }
 
-        {orderedElements}
-        <div className="order-summary">
+          {orderedElements}
+
           {serviceProps.deliveryProps && serviceProps.deliveryProps.deliveryPrice ?
             <p className="order-summary-entry clearfix">
               <span className="option-title">配送费:</span>
@@ -83,30 +84,28 @@ module.exports = React.createClass({
             :
             false
           }
-          <div className="options-group">
-            {serviceProps.couponsProps.couponsList && serviceProps.couponsProps.couponsList.length
-              && helper.getCouponsLength(serviceProps.couponsProps.couponsList) !== 0 && commercialProps.diningForm !== 0 ?
-              <a className="option" href="#coupon-select">
-                <span className="option-title">使用优惠券</span>
-                <span className="badge-coupon">
-                  {serviceProps.couponsProps.inUseCoupon ?
-                    '已使用一张优惠券'
-                    :
-                    `${helper.getCouponsLength(serviceProps.couponsProps.couponsList)}张可用`
-                  }
-                </span>
-                <span className="option-btn btn-arrow-right">{serviceProps.couponsProps.inUseCoupon ? false : '未使用'}</span>
-              </a>
-            : false}
-            {serviceProps.integralsInfo && commercialProps.diningForm !== 0 ?
-              <ActiveSelect
-                optionsData={[serviceProps.integralsInfo]} onSelectOption={setOrderProps}
-                optionComponent={OrderPropOption}
-              />
-            : false}
-          </div>
+          {serviceProps.couponsProps.couponsList && serviceProps.couponsProps.couponsList.length
+            && helper.getCouponsLength(serviceProps.couponsProps.couponsList) !== 0 && commercialProps.diningForm !== 0 ?
+            <a className="option" href="#coupon-select">
+              <span className="option-title">使用优惠券</span>
+              <span className="badge-coupon">
+                {serviceProps.couponsProps.inUseCoupon ?
+                  '已使用一张优惠券'
+                  :
+                  `${helper.getCouponsLength(serviceProps.couponsProps.couponsList)}张可用`
+                }
+              </span>
+              <span className="option-btn btn-arrow-right">{serviceProps.couponsProps.inUseCoupon ? false : '未使用'}</span>
+            </a>
+          : false}
+          {serviceProps.integralsInfo && commercialProps.diningForm !== 0 ?
+            <ActiveSelect
+              optionsData={[serviceProps.integralsInfo]} onSelectOption={setOrderProps}
+              optionComponent={OrderPropOption}
+            />
+          : false}
           {commercialProps.carryRuleVO && helper.clearSmallChange(commercialProps.carryRuleVO, dishesPrice, serviceProps).smallChange < 0 ?
-            <p className="order-summary-entry clearfix">
+            <p className="option clearfix">
               <span className="option-title">尾数调整:</span>
               <span className="order-discount price">
                 {Math.abs(helper.clearSmallChange(commercialProps.carryRuleVO, dishesPrice, serviceProps).smallChange)}
@@ -116,124 +115,126 @@ module.exports = React.createClass({
             false
           }
         </div>
-        <div className="order-summary">
-          {serviceProps.discountProps.inUseDiscount ?
-            <p className="order-summary-entry clearfix">
-              <span className="option-title option-title--icon order-summary-icon1">会员优惠:</span>
-              <span className="order-discount discount">
-                {serviceProps.discountProps.inUseDiscount}
-              </span>
-            </p>
-            :
-            false
-          }
-          {serviceProps.activityBenefit.benefitMoney > 0 ?
-            <p className="order-summary-entry clearfix">
-              <span className="option-title option-title--icon order-summary-icon1">活动优惠:</span>
-              <span className="order-discount discount">
-                {serviceProps.activityBenefit.benefitMoney}
-              </span>
-            </p>
-            :
-            false
-          }
-          {serviceProps.deliveryProps && serviceProps.deliveryProps.freeDeliveryPrice >= 0 && serviceProps.deliveryProps.deliveryPrice
-            && dishesPrice >= serviceProps.deliveryProps.freeDeliveryPrice && serviceProps.deliveryProps.deliveryPrice !== 0 ?
-            <p className="order-summary-entry clearfix">
-              <span className="option-title option-title--icon order-summary-icon2">满{serviceProps.deliveryProps.freeDeliveryPrice}元减免配送费</span>
-              <span className="order-discount discount">
-                {serviceProps.deliveryProps.deliveryPrice}
-              </span>
-            </p>
-            :
-            false
-          }
-          {serviceProps.couponsProps.inUseCoupon ?
-            <p className="order-summary-entry clearfix">
-              <span className="option-title option-title--icon order-summary-icon3">优惠券优惠:</span>
-              <span className="order-discount discount">
-                {serviceProps.discountProps.discountInfo && serviceProps.discountProps.discountInfo.isChecked ?
-                  formatPrice(
-                    helper.countPriceByCoupons(
-                      serviceProps.couponsProps.inUseCouponDetail,
-                      helper.getPriceCanBeUsedToBenefit(dishesPrice, serviceProps.deliveryProps)
-                        - serviceProps.discountProps.inUseDiscount - Number(serviceProps.activityBenefit.benefitMoney)
+        <div className="options-group">
+          <div className="order-summary">
+            {serviceProps.discountProps.inUseDiscount ?
+              <p className="order-summary-entry clearfix">
+                <span className="option-title option-title--icon order-summary-icon1">会员优惠:</span>
+                <span className="order-discount discount">
+                  {serviceProps.discountProps.inUseDiscount}
+                </span>
+              </p>
+              :
+              false
+            }
+            {serviceProps.activityBenefit.benefitMoney > 0 ?
+              <p className="order-summary-entry clearfix">
+                <span className="option-title option-title--icon order-summary-icon1">活动优惠:</span>
+                <span className="order-discount discount">
+                  {serviceProps.activityBenefit.benefitMoney}
+                </span>
+              </p>
+              :
+              false
+            }
+            {serviceProps.deliveryProps && serviceProps.deliveryProps.freeDeliveryPrice >= 0 && serviceProps.deliveryProps.deliveryPrice
+              && dishesPrice >= serviceProps.deliveryProps.freeDeliveryPrice && serviceProps.deliveryProps.deliveryPrice !== 0 ?
+              <p className="order-summary-entry clearfix">
+                <span className="option-title option-title--icon order-summary-icon2">满{serviceProps.deliveryProps.freeDeliveryPrice}元减免配送费</span>
+                <span className="order-discount discount">
+                  {serviceProps.deliveryProps.deliveryPrice}
+                </span>
+              </p>
+              :
+              false
+            }
+            {serviceProps.couponsProps.inUseCoupon ?
+              <p className="order-summary-entry clearfix">
+                <span className="option-title option-title--icon order-summary-icon3">优惠券优惠:</span>
+                <span className="order-discount discount">
+                  {serviceProps.discountProps.discountInfo && serviceProps.discountProps.discountInfo.isChecked ?
+                    formatPrice(
+                      helper.countPriceByCoupons(
+                        serviceProps.couponsProps.inUseCouponDetail,
+                        helper.getPriceCanBeUsedToBenefit(dishesPrice, serviceProps.deliveryProps)
+                          - serviceProps.discountProps.inUseDiscount - serviceProps.activityBenefit.benefitMoney
+                      )
                     )
-                  )
-                  :
-                  formatPrice(
-                    helper.countPriceByCoupons(
-                      serviceProps.couponsProps.inUseCouponDetail,
-                      helper.getPriceCanBeUsedToBenefit(dishesPrice, serviceProps.deliveryProps)
-                        - Number(serviceProps.activityBenefit.benefitMoney)
+                    :
+                    formatPrice(
+                      helper.countPriceByCoupons(
+                        serviceProps.couponsProps.inUseCouponDetail,
+                        helper.getPriceCanBeUsedToBenefit(dishesPrice, serviceProps.deliveryProps)
+                          - serviceProps.activityBenefit.benefitMoney
+                      )
                     )
-                  )
-                }
-              </span>
-            </p>
-            :
-            false
-          }
+                  }
+                </span>
+              </p>
+              :
+              false
+            }
 
-          {serviceProps.integralsInfo && serviceProps.integralsInfo.isChecked && commercialProps.carryRuleVO ?
-            <p className="order-summary-entry clearfix">
-              <span className="option-title option-title--icon order-summary-icon4">积分抵扣:</span>
-              <span className="order-discount discount">
-                {helper.countIntegralsToCash(
-                  Number(helper.countPriceWithCouponAndDiscount(dishesPrice, serviceProps)),
-                  serviceProps.integralsDetail
-                ).commutation}
-              </span>
-              <span className="order-integral">
-                {helper.countIntegralsToCash(
-                  Number(helper.countPriceWithCouponAndDiscount(dishesPrice, serviceProps)),
-                  serviceProps.integralsDetail
-                ).integralInUsed}
-              </span>
-            </p>
-            :
-            false
-          }
-          {commercialProps.carryRuleVO && helper.clearSmallChange(commercialProps.carryRuleVO, dishesPrice, serviceProps).smallChange > 0 ?
-            <p className="order-summary-entry clearfix">
-              <span className="option-title option-title--icon order-summary-icon5">自动抹零:</span>
-              <span className="order-discount discount">
-                {Math.abs(helper.clearSmallChange(commercialProps.carryRuleVO, dishesPrice, serviceProps).smallChange)}
-              </span>
-            </p>
-            :
-            false
-          }
-        </div>
-        <div className="option order-total clearfix">
-          <div className="order-total-left">
-            <span className="text-dove-grey">总计: </span>
-            <span className="price">{
-              helper.countTotalPriceWithoutBenefit(dishesPrice, serviceProps.deliveryProps)
-            }</span>
+            {serviceProps.integralsInfo && serviceProps.integralsInfo.isChecked && commercialProps.carryRuleVO ?
+              <p className="order-summary-entry clearfix">
+                <span className="option-title option-title--icon order-summary-icon4">积分抵扣:</span>
+                <span className="order-discount discount">
+                  {helper.countIntegralsToCash(
+                    Number(helper.countPriceWithCouponAndDiscount(dishesPrice, serviceProps)),
+                    serviceProps.integralsDetail
+                  ).commutation}
+                </span>
+                <span className="order-integral">
+                  {helper.countIntegralsToCash(
+                    Number(helper.countPriceWithCouponAndDiscount(dishesPrice, serviceProps)),
+                    serviceProps.integralsDetail
+                  ).integralInUsed}
+                </span>
+              </p>
+              :
+              false
+            }
+            {commercialProps.carryRuleVO && helper.clearSmallChange(commercialProps.carryRuleVO, dishesPrice, serviceProps).smallChange > 0 ?
+              <p className="order-summary-entry clearfix">
+                <span className="option-title option-title--icon order-summary-icon5">自动抹零:</span>
+                <span className="order-discount discount">
+                  {Math.abs(helper.clearSmallChange(commercialProps.carryRuleVO, dishesPrice, serviceProps).smallChange)}
+                </span>
+              </p>
+              :
+              false
+            }
           </div>
-          {commercialProps.carryRuleVO ?
-            <div>
-              <div className="order-total-left">
-                <span className="text-dove-grey">优惠: </span>
-                <span className="price">
-                  {
-                    helper.countDecreasePrice(orderedDishesProps, serviceProps, commercialProps)
-                  }
-                </span>
-              </div>
-              <div className="order-total-right">
-                <span className="text-dove-grey">实付: </span>
-                <span className="price">
-                  {
-                    helper.countFinalNeedPayMoney(orderedDishesProps, serviceProps, commercialProps)
-                  }
-                </span>
-              </div>
+          <div className="option order-total clearfix">
+            <div className="order-total-left">
+              <span className="text-dove-grey">总计: </span>
+              <span className="price">{
+                helper.countTotalPriceWithoutBenefit(dishesPrice, serviceProps.deliveryProps)
+              }</span>
             </div>
-            :
-            false
-          }
+            {commercialProps.carryRuleVO ?
+              <div>
+                <div className="order-total-left">
+                  <span className="text-dove-grey">优惠: </span>
+                  <span className="price">
+                    {
+                      helper.countDecreasePrice(orderedDishesProps, serviceProps, commercialProps)
+                    }
+                  </span>
+                </div>
+                <div className="order-total-right">
+                  <span className="text-dove-grey">实付: </span>
+                  <span className="price">
+                    {
+                      helper.countFinalNeedPayMoney(orderedDishesProps, serviceProps, commercialProps)
+                    }
+                  </span>
+                </div>
+              </div>
+              :
+              false
+            }
+          </div>
         </div>
       </div>
     );
