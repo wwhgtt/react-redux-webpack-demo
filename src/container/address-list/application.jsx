0c5856158b29927ca63961ab88689a68103f9524
杨 丹 <yangd@shishike.com>
2016-10-31 15:02:31 +0800

    taskID:6726 workHour:1 remainHour:999 comment:地址管理优化

--- src/container/address-list/application.jsx
+++ src/container/address-list/application.jsx
@@ -21,6 +21,7 @@ const AddressListApplication = React.createClass({
   getInitialState() {
     return {
       allAddressList: [],
+      isShowTip: false,
     };
   },
   componentDidMount() {
@@ -32,8 +33,17 @@ const AddressListApplication = React.createClass({
   },
   onAddressSelect(evt, option) {
     const dataset = evt.target.dataset;
+    const { allAddressList } = this.state;
     const { setSessionAndForwardEditUserAddress } = this.props;
     if (!dataset.hasOwnProperty('editor')) {
+      return;
+    }
+
+    if (allAddressList.length >= 10) {
+      this.setState({ isShowTip: true });
+      setTimeout(() => {
+        this.setState({ isShowTip: false });
+      }, 5000);
       return;
     }
 
@@ -74,21 +84,27 @@ const AddressListApplication = React.createClass({
           onSelectOption={this.onAddressSelect}
         />
       );
-    }
-    if (allAddressList.length < 10) {
+    } else {
       elems.push(
-        <a key="add" className="address-add-more btn--yellow flex-none" onTouchTap={this.onAddressSelect} data-editor="">新增地址</a>
+        <div className="address-no flex-rest" key="noAddress">
+          <div className="address-no-img"></div>
+          <p className="address-no-title">主人还没有收货地址 <br />快来添加一个吧～</p>
+        </div>
       );
     }
     return elems;
   },
   render() {
+    const { isShowTip } = this.state;
     return (
       <div className="address flex-columns">
+      {isShowTip &&
         <div className="address-comments ellipsis flex-none">
           最多可保存10个常用地址，还需新增，请删除或修改已有地址
         </div>
+      }
         {this.buildAddressElement()}
+        <a key="add" className="address-add-more btn--yellow flex-none" onTouchTap={this.onAddressSelect} data-editor="">新增地址</a>
       </div>
     );
   },
