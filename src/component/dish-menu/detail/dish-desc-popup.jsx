c5f81089e676196d1086d4c3112d04f3641bf86a
wenhai wu <wuwh@shishike.com>
2016-11-03 21:29:58 +0800

    taskID:7728 workHour:1 remainHour:2 comment:fix

--- src/component/dish-menu/detail/dish-desc-popup.jsx
+++ src/component/dish-menu/detail/dish-desc-popup.jsx
@@ -25,9 +25,8 @@ module.exports = React.createClass({
     } else if (dish.isMember && dish.discountType === 2) {
       memberPrice = dish.memberPrice;
     }
-    const loginTypeStr = dish.loginType.toString();
     let registerUrl = `http://${location.host}/member/register?shopId=${shopId}&returnUrl=${encodeURIComponent(location.href)}`;
-    if (loginTypeStr === '1') {
+    if (dish.loginType && +dish.loginType === 1) {
       // 微信登录
       registerUrl = `http://${location.host}/user/validBindMobile?shopId=${shopId}&returnUrl=${encodeURIComponent(location.href)}`;
     }
