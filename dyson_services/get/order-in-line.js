38a2e15178092d5d8ea12c6819523aa2bb6cc995
wenhai wu <wuwh@shishike.com>
2016-11-07 17:14:08 +0800

    taskID:7728 workHour:1 remainHour:2 comment:fix

--- dyson_services/get/order-in-line.js
+++ dyson_services/get/order-in-line.js
@@ -7,7 +7,7 @@ module.exports = {
       shopId:247900001, // 门店ID
       shopName:'南粉北面南粉北面高攀路店面', // 门店名称
       shopLogo:'http://kry-test-2.qiniudn.com/5e9008fc4e9143e1b0e28fe60ec87470.png?imageView/2/w/512/h/512', // 门店logo
-      openStatus:'商家设备未联网', // 营业状态
+      openStatus:'营业中', // 营业状态
       orderId:null, // 订单ID
       queueTip:false, // 是否已排队
       name:'啦啦啦', // 姓名
@@ -29,6 +29,38 @@ module.exports = {
           count:0,
           maxNum:null,
           queueLineId:10888,
+          minPersonCount:5,
+          maxPersonCount:10,
+        },
+        {
+          queueName:'队列A', // 队列名称
+          count:0, // 等位数
+          maxNum:null, // 当前排队号
+          queueLineId:10322387, // 门店排队ID
+          minPersonCount:1, // 队列最小就餐人数
+          maxPersonCount:1, // 队列最大就餐人数, 为0表示无上限
+        },
+        {
+          queueName:'队列B',
+          count:0,
+          maxNum:null,
+          queueLineId:102328,
+          minPersonCount:5,
+          maxPersonCount:10,
+        },
+        {
+          queueName:'队列A', // 队列名称
+          count:0, // 等位数
+          maxNum:null, // 当前排队号
+          queueLineId:1232332387, // 门店排队ID
+          minPersonCount:1, // 队列最小就餐人数
+          maxPersonCount:1, // 队列最大就餐人数, 为0表示无上限
+        },
+        {
+          queueName:'队列B',
+          count:0,
+          maxNum:null,
+          queueLineId:10234248,
           minPersonCount:5,
           maxPersonCount:10,
         },
