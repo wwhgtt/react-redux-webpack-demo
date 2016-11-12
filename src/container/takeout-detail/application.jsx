3e8f016a5f29411c90277faa11f9182e69223c1f
杨 丹 <yangd@shishike.com>
2016-11-12 21:08:11 +0800

    taskID:6726 workHour:1 remainHour:999 comment:订单详情

--- src/container/takeout-detail/application.jsx
+++ src/container/takeout-detail/application.jsx
@@ -92,22 +92,22 @@ const TakeoutDetailApplication = React.createClass({
   render() {
     const { takeoutDetail } = this.props;
     return (
-      <div className="application">
+      <div className="application takeout-page">
         <div className="flex-columns">
-          <div className="flex-rest">
+          <div className="flex-rest detail-content">
             <div className="order-status">
-              <span className="order-status-title">{takeoutDetail.status}</span>
+              <span className="order-status-title ellipsis">{takeoutDetail.status}</span>
               {
                 takeoutDetail.tradeFailReason &&
-                  <span className="order-status-reason">{takeoutDetail.tradeFailReason}</span>
+                  <span className="order-status-detail ellipsis">{takeoutDetail.tradeFailReason}</span>
               }
               {takeoutDetail.status === '订单待支付' &&
-                <span className="order-status-detail">
+                <span className="order-status-detail ellipsis">
                   {this.formatCuntDown(this.state.countDown)}后订单自动取消
                 </span>
               }
             </div>
-            <div className="options-group">
+            <div className="options-group takeout-head">
               <div className="option">
                 <a className="shop-info" href={`http://${location.host}/takeaway/selectDish?shopId=${shopId}`}>
                   <img className="shop-info-logo" role="presentation" src={takeoutDetail.shopLogo || shopLogoDefault} />
@@ -117,23 +117,23 @@ const TakeoutDetailApplication = React.createClass({
               {
                 takeoutDetail.serialNo &&
                   <div className="option">
-                    <span>流水号</span>
-                    <span>{takeoutDetail.serialNo}</span>
+                    <span className="takeout-head-title takeout-head-no">流水号</span>
+                    <span className="takeout-head-content">{takeoutDetail.serialNo}</span>
                   </div>
               }
-              <div className="option">
-                <span>送达时间</span>
-                <span>{takeoutDetail.expectTime}</span>
+              <div className="option option-no-padding">
+                <span className="takeout-head-title">送达时间</span>
+                <span className="takeout-head-content">{takeoutDetail.expectTime}</span>
               </div>
-              <div className="option">
-                <span>收货地址</span>
-                <div>
+              <div className="option clearfix">
+                <span className="takeout-head-title fl">收货信息</span>
+                <div className="fl takeout-head-content">
                   <p>{takeoutDetail.address}</p>
-                  <span>{takeoutDetail.name}{this.getSex()}{takeoutDetail.mobile}</span>
+                  <p>{takeoutDetail.name}{this.getSex()} {takeoutDetail.mobile}</p>
                 </div>
               </div>
             </div>
-            <div className="options-group">
+            <div className="options-group option-min">
             {
               takeoutDetail.dishItems &&
                 takeoutDetail.dishItems.map((item, index) =>
@@ -141,7 +141,7 @@ const TakeoutDetailApplication = React.createClass({
               )
             }
             </div>
-            <div className="options-group">
+            <div className="options-group option-min option-extra">
               {
                 takeoutDetail.extraFee && takeoutDetail.extraFee.map((item, index) =>
                   <div className="option" key={index}>
@@ -151,13 +151,13 @@ const TakeoutDetailApplication = React.createClass({
                 )
               }
             </div>
-            <div className="list-default">
+            <div className="list-default option-privilege">
               {
                 takeoutDetail.tradePrivileges && takeoutDetail.tradePrivileges.map((item, index) =>
                   <div className="list-item flex-row" key={index}>
                     <span className={`icon-privilege icon-${Math.abs(item.privilegeType)}`}>{item.privilegeName}</span>
-                    {item.privilegeValue && <span>-{Math.abs(item.privilegeValue)}</span>}
-                    <div className="fr">-<span className="price">{Math.abs(item.privilegeAmount)}</span></div>
+                    {item.privilegeValue && <span className="list-privilege-value">-{Math.abs(item.privilegeValue)}</span>}
+                    <div className="fr list-privilege">-<span className="price">{Math.abs(item.privilegeAmount)}</span></div>
                   </div>
                 )
               }
@@ -165,25 +165,27 @@ const TakeoutDetailApplication = React.createClass({
                 Boolean(takeoutDetail.carryRuleAmount) && (
                   <div className="list-item flex-row">
                     <span className="icon-privilege icon-carry">自动抹零</span>
-                    <div className="fr">
+                    <div className="fr list-privilege">
                       {takeoutDetail.carryRuleAmount < 0 ? '-' : ''}<span className="price">{Math.abs(takeoutDetail.carryRuleAmount)}</span>
                     </div>
                   </div>
                 )
               }
-              <div className="list-item">
-                <div className="flex-row">
-                  <div className="flex-row-item">原价
-                    <span className="price">{(takeoutDetail.tradeAmount || 0) + Math.abs(takeoutDetail.tradePrivilegeAmount || 0)}</span>
-                  </div>
-                  <div className="flex-row-item">共优惠<span className="price">{Math.abs(takeoutDetail.tradePrivilegeAmount || 0)}</span></div>
-                  <div className="flex-row-item">总计：
-                    <span className="price">{takeoutDetail.tradeAmount}</span>
-                  </div>
+            </div>
+            <div className="list-statictis">
+              <div className="flex-row">
+                <div className="flex-row-item list-statictis-title">原价
+                  <span className="price">{(takeoutDetail.tradeAmount || 0) + Math.abs(takeoutDetail.tradePrivilegeAmount || 0)}</span>
+                </div>
+                <div className="flex-row-item list-statictis-title">共优惠
+                  <span className="price">{Math.abs(takeoutDetail.tradePrivilegeAmount || 0)}</span>
+                </div>
+                <div className="flex-row-item list-statictis-title">总计：
+                  <span className="price">{takeoutDetail.tradeAmount}</span>
                 </div>
               </div>
             </div>
-            <p>其他信息</p>
+            <p className="list-other">其他信息</p>
             <div className="option-group">
               <div className="list-default">
                 <div className="list-item">
@@ -221,7 +223,7 @@ const TakeoutDetailApplication = React.createClass({
               {(takeoutDetail.status === '订单待支付' || takeoutDetail.status === '订单支付失败') &&
                 <a
                   className="btn-oparate-count"
-                  href={`http://${location.host}/shop/payDetail?shopId=${shopId}&orderId=${takeoutDetail.orderId}&orderType=TS`}
+                  href={`http://${location.host}/shop/payDetail?shopId=${shopId}&orderId=${takeoutDetail.orderId}&orderType=WM`}
                 >结账</a>
               }
             </div>
