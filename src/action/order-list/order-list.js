d2594603405055d848432f3d638ff4a97375c771
杨 丹 <yangd@shishike.com>
2016-11-16 15:42:13 +0800

    taskID:6726 workHour:1 remainHour:999 comment:订单中心优化

--- src/action/order-list/order-list.js
+++ src/action/order-list/order-list.js
@@ -11,20 +11,24 @@ const setQueueList = createAction('SET_QUEUE_LIST', queueList => queueList);
 
 exports.setChildView = createAction('SET_CHILD_VIEW', childView => childView);
 const setErrorMsg = exports.setErrorMsg = createAction('SET_ERROR_MSG', error => error);
+const setLoadStatus = createAction('SET_LOAD_STATUS', loadStatus => loadStatus);
 
 const shopId = helper.getUrlParam('shopId');
 
 // 堂食订单列表
 exports.getOrderList = (pageNum) => (dispatch, getStates) => {
+  dispatch(setLoadStatus(true));
   const orderListURL = `${config.orderListAPI}?shopId=${shopId}&pageNum=${pageNum}`;
   fetch(orderListURL, config.requestOptions).
   then(res => {
     if (!res.ok) {
       dispatch(setErrorMsg('订单列表获取失败'));
+      dispatch(setLoadStatus(false));
     }
     return res.json();
   }).
   then(res => {
+    dispatch(setLoadStatus(false));
     if (res.code === '200') {
       dispatch(setOrderList(res.data.list));
     } else {
@@ -35,11 +39,13 @@ exports.getOrderList = (pageNum) => (dispatch, getStates) => {
 
 // 外卖订单列表
 exports.getTakeOutList = (pageNum) => (dispatch, getStates) => {
+  dispatch(setLoadStatus(true));
   const takeOutListURL = `${config.takeOutListAPI}?shopId=${shopId}&pageNum=${pageNum}`;
   fetch(takeOutListURL, config.requestOptions).
   then(res => {
     if (!res.ok) {
       dispatch(setErrorMsg('订单列表获取失败'));
+      dispatch(setLoadStatus(false));
     }
     return res.json();
   }).
@@ -49,16 +55,19 @@ exports.getTakeOutList = (pageNum) => (dispatch, getStates) => {
     } else {
       dispatch(setErrorMsg('订单列表获取失败'));
     }
+    dispatch(setLoadStatus(false));
   });
 };
 
 // 预订订单列表
 exports.getBookList = (pageNum) => (dispatch, getStates) => {
+  dispatch(setLoadStatus(true));
   const bookListURL = `${config.bookListAPI}?shopId=${shopId}&pageNum=${pageNum}`;
   fetch(bookListURL, config.requestOptions).
   then(res => {
     if (!res.ok) {
       dispatch(setErrorMsg('订单列表获取失败'));
+      dispatch(setLoadStatus(false));
     }
     return res.json();
   }).
@@ -68,16 +77,19 @@ exports.getBookList = (pageNum) => (dispatch, getStates) => {
     } else {
       dispatch(setErrorMsg('订单列表获取失败'));
     }
+    dispatch(setLoadStatus(false));
   });
 };
 
 // 排队订单列表
 exports.getQueueList = (pageNum) => (dispatch, getStates) => {
+  dispatch(setLoadStatus(true));
   const queueListURL = `${config.queueListAPI}?shopId=${shopId}&pageNum=${pageNum}`;
   fetch(queueListURL, config.requestOptions).
   then(res => {
     if (!res.ok) {
       dispatch(setErrorMsg('订单列表获取失败'));
+      dispatch(setLoadStatus(false));
     }
     return res.json();
   }).
@@ -87,5 +99,6 @@ exports.getQueueList = (pageNum) => (dispatch, getStates) => {
     } else {
       dispatch(setErrorMsg('订单列表获取失败'));
     }
+    dispatch(setLoadStatus(false));
   });
 };
