71641a8e7b00fcda4c35e3a39378f506b6cef52c
AJ Frank <xuhj@shishike.com>
2016-11-22 14:49:06 +0800

    修正BUG

--- src/action/book-check-order/book-check-order.js
+++ src/action/book-check-order/book-check-order.js
@@ -10,6 +10,9 @@ const shopId = commonHelper.getUrlParam('shopId');
 // const orderId = commonHelper.getUrlParam('orderId');
 
 exports.getBookCheckOrder = () => (dispatch, getState) => {
+  if (!localStorage.lastOrderedDishes) { // 如果没有菜品
+    location.href = `${config.orderallListURL}&shopId=${shopId}#dinner`;
+  }
   const lastOrderedDishes = JSON.parse(localStorage.lastOrderedDishes || '{}');
   dispatch(setOrderDetail(lastOrderedDishes));
 };
@@ -28,12 +31,12 @@ exports.confirmBill = (data) => (dispatch, getstate) => {
     return res.json();
   }).
   then(res => {
-    dispatch(setLoadMsg({ status:false, word:'' }));
     if (res.code === '200') {
       const orderId = sessionStorage.YDrelatedId;
       localStorage.clear();
       location.href = `${config.bookingDetailURL}?shopId=${commonHelper.getUrlParam('shopId')}&type=YD&orderId=${orderId}`;
     } else {
+      dispatch(setLoadMsg({ status:false, word:'' }));
       dispatch(setErrorMsg(res.msg));
     }
   }).
