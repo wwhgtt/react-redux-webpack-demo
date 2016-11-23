2f37fe7bff1e54dbc54a7237c1458adc576006cc
AJ Frank <xuhj@shishike.com>
2016-11-23 17:52:25 +0800

    优化预点菜

--- src/component/book/book-info-main-item.jsx
+++ src/component/book/book-info-main-item.jsx
@@ -139,8 +139,8 @@ module.exports = React.createClass({ // ShowBasicInfo
     const { expand } = this.state;
     const mainPrice = this.getMainPrice(mainDish);
     const mainNum = this.getMainNum(mainDish);
-    const getMainPropertyTypeBrief = this.getMainPropertyTypeBrief(mainDish);
-    const getMainDishIngredientBrief = this.getMainDishIngredientBrief(mainDish);
+    const mainPropertyTypeBrief = this.getMainPropertyTypeBrief(mainDish);
+    const mainDishIngredientBrief = this.getMainDishIngredientBrief(mainDish);
     const hasPropertyTypeList = mainDish.propertyTypeList && mainDish.propertyTypeList.filter(property => property.type !== 4).length > 0;
     const hasDishIngredientInfos = mainDish.dishIngredientInfos && mainDish.dishIngredientInfos.length > 0;
     const hasSubDishItems = mainDish.subDishItems && mainDish.subDishItems.length > 0;
@@ -157,16 +157,16 @@ module.exports = React.createClass({ // ShowBasicInfo
             })}
         >
           {mainDish.name}
-          {getMainPropertyTypeBrief.norms}
+          {mainPropertyTypeBrief.norms}
           {mainDish.unitName ? `/${mainDish.unitName}` : ''}
         </div>
         {
           expand && mainDish.type === 0 && (hasPropertyTypeList || hasDishIngredientInfos) && (
             <div className="option-brief ellipsis">
-              {getMainPropertyTypeBrief.propertyType} &nbsp;
+              {mainPropertyTypeBrief.propertyType} &nbsp;
               {
-                getMainDishIngredientBrief &&
-                  `配料:${getMainDishIngredientBrief}`
+                mainDishIngredientBrief &&
+                  `配料:${mainDishIngredientBrief}`
               }
             </div>
           )
