f2310173a7345cfdc6d93485b0c5d80d2e13b755
wenhai wu <wuwh@shishike.com>
2016-12-18 22:45:13 +0800

    taskID:11240 workHour:1 remainHour:2 comment:fix

--- src/component/dish-menu/dish-list-item.jsx
+++ src/component/dish-menu/dish-list-item.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const classnames = require('classnames');
 const Counter = require('../mui/counter.jsx');
-const shallowCompare = require('react-addons-shallow-compare');
+// const shallowCompare = require('react-addons-shallow-compare');
 const helper = require('../../helper/dish-helper');
 const imagePlaceholder = require('../../asset/images/dish-placeholder.png');
 const _find = require('lodash.find');
@@ -19,9 +19,9 @@ module.exports = React.createClass({
     marketList: React.PropTypes.object,
     theme: React.PropTypes.string,
   },
-  shouldComponentUpdate(nextProps, nextState) {
-    return shallowCompare(this, nextProps, nextState);
-  },
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return shallowCompare(this, nextProps, nextState);
+  // },
   componentDidUpdate() {
   },
   onBtnTap(newCount, increment) {
